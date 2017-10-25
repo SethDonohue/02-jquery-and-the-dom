0d98579c40bd472aacf7b29c30356a30ed51bcbc
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2017-10-25 10:40:33 -0700

    Finished all the article filling code

--- dustin-seth/scripts/article.js
+++ dustin-seth/scripts/article.js
@@ -22,19 +22,24 @@ Article.prototype.toHtml = function() {
   // Cloning the article gives you a full copy to manipulate in the javascript instead of manipulating the html DOM. You can modify the clone before re-inserting it into the document.
 
   let $newArticle = $('article.template').clone();
-  /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
+  /* TODONE: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
+
+  $newArticle.removeClass('template');
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.data('category', this.category);
 
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
-    We need to fill in:
-      1. author name,
-      2. author url,
-      3. article title,
-      4. article body, and
-      5. publication date. */
-
+  /* TODONE: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
+    We need to fill in:*/
+  // 1. author name,
+  $newArticle.find('a').text(this.author);
+  // 2. author url,
+  $newArticle.find('a').attr('href',this.authorUrl);
+  // 3. article title,
+  $newArticle.find('h1').text(this.title);
+  // 4. article body, and
+  $newArticle.find('section.article-body').append(`<p>${this.body}</p>`);
+  // 5. publication date.
   // REVIEW: Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   $newArticle.append('<hr>');
